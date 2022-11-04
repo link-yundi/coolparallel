f15e3eb2bfdc783f7254ce67775377c97cf196f0
yundi <yundi.xxii@outlook.com>
2022-11-04 09:17:27 +0800

    task arg

--- pool.go
+++ pool.go
@@ -12,34 +12,34 @@ Created on 2022-11-03 22:25
 
 // ========================== 协程池 ==========================
 type ParallelPool struct {
-	work chan func() // 任务
-	num  chan int    // 数量
+	work chan func(arg any) // 任务
+	num  chan int           // 数量
 	wg   *sync.WaitGroup
 }
 
 func NewParallelPool(size int) *ParallelPool {
 	return &ParallelPool{
-		work: make(chan func()),
+		work: make(chan func(arg any)),
 		num:  make(chan int, size),
 		wg:   &sync.WaitGroup{},
 	}
 }
 
 // 往协程池中添加任务
-func (pp *ParallelPool) AddTask(task func()) {
+func (pp *ParallelPool) AddTask(task func(arg any), arg any) {
 	select {
 	case pp.work <- task:
 		pp.wg.Add(1)
 	case pp.num <- 1:
 		pp.wg.Add(1)
-		go pp.worker(task)
+		go pp.worker(task, arg)
 	}
 }
 
 // 执行任务
-func (pp *ParallelPool) worker(task func()) {
+func (pp *ParallelPool) worker(task func(arg any), arg any) {
 	for {
-		task()
+		task(arg)
 		pp.wg.Done()
 		task = <-pp.work
 	}
--- pool_test.go
+++ pool_test.go
@@ -17,12 +17,12 @@ Created on 2022-11-03 23:52
 // ========================== 协程池 ==========================
 func TestPool(t *testing.T) {
 	p := NewParallelPool(8)
-	task := func() {
+	task := func(arg any) {
 		time.Sleep(2 * time.Second)
 		ylog.Info(time.Now())
 	}
 	for i := 0; i < 32; i++ {
-		p.AddTask(task)
+		p.AddTask(task, nil)
 	}
 	p.Wait()
 }
